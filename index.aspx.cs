a0212787d9e78c72f20a914d4cb9945c1b4d1a82
siwei0240 <48469968+siwei0240@users.noreply.github.com>
2019-04-07 22:13:03 +0800

    首页控件设计

---
+++ index.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        bindFreight();
+        bindTruck();
+        bindSpecial();        
+        bindDepot();       
+    }
+   
+    //绑定货源
+    protected void bindFreight()
+    {
+        string sql = "select top 10 * from tb_Freight where Auditing = '1' order by ID DESC";
+        gvFreight.DataSource = dataOperate.getDataset(sql, "tb_Freight");
+        gvFreight.DataBind();
+    }
+    //绑定车源
+    protected void bindTruck()
+    {
+        string sql = "select top 10 * from tb_Truck where Auditing = '1' order by ID DESC";
+        gvTruck.DataSource = dataOperate.getDataset(sql, "tb_Truck");
+        gvTruck.DataBind();
+    }
+    //绑定专线
+    protected void bindSpecial()
+    {
+        string sql = "select top 10 * from tb_Special where Auditing = '1' order by ID DESC";
+        gvSpecial.DataSource = dataOperate.getDataset(sql, "tb_Special");
+        gvSpecial.DataBind();
+    }
+   
+    //绑定仓储
+    protected void bindDepot()
+    {
+        string sql = "select top 10 * from tb_Depot where Auditing = '1' order by ID DESC";
+        gvDepot.DataSource = dataOperate.getDataset(sql, "tb_Depot");
+        gvDepot.DataBind();
+    }
+
+
+   
+}
