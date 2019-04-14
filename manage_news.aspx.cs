2c81c7bcedd4ad6baa13c71a9d916b0ba6a54c1e
Rebecca-wangwanxin <48469820+Rebecca-wangwanxin@users.noreply.github.com>
2019-04-14 22:41:40 +0800

    新闻页的设计

---
+++ manage_news.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class Manage_manage_news : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        bindNews();
+    }
+    public void bindNews()
+    {
+        string sql = "select * from tb_News";
+        gvNews.DataSource = dataOperate.getDataset(sql,"tb_News");
+        gvNews.DataKeyNames = new string[] { "ID" };
+        gvNews.DataBind();
+    }
+    protected void gvNews_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        string sql = "delete from tb_News where ID = " + gvNews.DataKeys[e.RowIndex].Value.ToString();
+        if (dataOperate.execSQL(sql))
+        {
+            Page.RegisterStartupScript("true", "<script>alert('删除成功！')</script>");
+        }
+        else
+        {
+            Page.RegisterStartupScript("false", "<script>alert('删除失败！')</script>");
+        }
+     
+    }
+}
