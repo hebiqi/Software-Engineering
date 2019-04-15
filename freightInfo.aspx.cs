37137cd62f84fbc8fc3ced71c99675f1bb7dd066
Y-sailboat <48469886+Y-sailboat@users.noreply.github.com>
2019-04-15 16:54:34 +0800

    货源详细信息页

---
+++ freightInfo.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+
+public partial class freightInfo : System.Web.UI.Page
+{
+    protected string Start, Terminal, FreightType, FreightWeight, Linkman, Phone, Term, Content, FBDate;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        bindFreightInfo();
+    }
+
+    protected void bindFreightInfo()
+    {
+        string ID = Request.QueryString["ID"].ToString();
+        string sql = "select * from tb_Freight where ID=" + ID;
+        SqlDataReader sdr = dataOperate.getRow(sql);
+        sdr.Read();
+        Start = sdr["start"].ToString();
+        Terminal = sdr["Terminal"].ToString();
+        FreightType = sdr["FreightType"].ToString();
+        FreightWeight = sdr["FreightWeight"].ToString() + sdr["WeightUnit"].ToString();
+        Linkman = sdr["Linkman"].ToString();
+        Phone = sdr["Phone"].ToString();
+        Term = Convert.ToDateTime(sdr["Term"]).ToLongDateString();
+        Content = sdr["Content"].ToString();
+        FBDate = Convert.ToDateTime(sdr["FBDate"]).ToLongDateString();
+
+    }
+
+}
