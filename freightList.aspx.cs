e7c2f1a1438ab4239489d4d37b598575d6fa8cd2
Y-sailboat <48469886+Y-sailboat@users.noreply.github.com>
2019-04-15 16:54:12 +0800

    货源信息页

---
+++ freightList.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class freightList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        bindFreight();//自定义方法
+    }
+
+    protected void bindFreight()
+    {
+        string sql = "select * from tb_Freight where Auditing = '1' order by ID DESC";
+        gvFreight.DataSource = dataOperate.getDataset(sql, "tb_Freight");   //调用getDataset方法将返回值绑定到GridView上
+        gvFreight.DataBind();                                               
+    }
+
+    protected void gvFreight_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvFreight.PageIndex = e.NewPageIndex;
+        gvFreight.DataBind();
+    }
+}
