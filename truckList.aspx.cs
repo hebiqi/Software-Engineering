df59ab8695173a2aa4e031f3d3ff3efeda10b370
Y-sailboat <48469886+Y-sailboat@users.noreply.github.com>
2019-04-15 16:51:27 +0800

    车源信息页

---
+++ truckList.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class truckList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        bindTruck();
+    }
+
+    protected void bindTruck()
+    {
+        string sql = "select * from tb_Truck where Auditing = '1' order by ID DESC";
+        gvTruck.DataSource = dataOperate.getDataset(sql, "tb_Truck");
+        gvTruck.DataBind();
+    }
+    protected void gvTruck_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvTruck.PageIndex = e.NewPageIndex;
+        gvTruck.DataBind();
+    }
+}
