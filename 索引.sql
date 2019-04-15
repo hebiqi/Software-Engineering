bf28adb87691455366f2bff7a82c0a000fe27cb6
hebiqi <48470631+hebiqi@users.noreply.github.com>
2019-04-15 20:36:33 +0800

    创建索引对象文件

---
+++ 索引.sql
@@ -0,0 +1,9 @@
+use WuLiu;
+CREATE UNIQUE INDEX Admin_Name
+ON Admin(Name);
+CREATE UNIQUE INDEX EUser_Name
+ON EUser(Name);
+CREATE UNIQUE INDEX News_Title
+ON News(Title);
+CREATE UNIQUE INDEX Recruit_Position
+ON Recruit(Position);
